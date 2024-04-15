6efa66c4c13e6ab1e286b099d6415c22bbe98bf9
nikodemmadziar <nnmadziarallegro@gmail.com>
2024-04-15 13:56:21 +0200

    funkcja tworzenia tablicy

--- powtorzeniowe/powtorzeniowe/Program.cs
+++ powtorzeniowe/powtorzeniowe/Program.cs
@@ -8,6 +8,14 @@ class PlikGłówny
         //zmienna ktora jest odwolaniem do innego skryptu
         tablice tablice = new tablice();
 
-        tablice.Wyswietl();
+        int[] tab = tablice.Tworzenie_tablicy();
+
+        for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine(tab[i]);
+        }
+        
+
+
     }
 }
--- powtorzeniowe/powtorzeniowe/tablice.cs
+++ powtorzeniowe/powtorzeniowe/tablice.cs
@@ -12,5 +12,22 @@ namespace powtorzeniowe
         {
             Console.WriteLine("to jest plik z tablicami");
         }
-    }
+        public int[] Tworzenie_tablicy()
+
+        {
+            Console.WriteLine("ile znakow ma miec tablica?");
+            int dl = int.Parse(Console.ReadLine());
+
+            int[] ints = new int[dl];
+
+            for (int i = 0; i < dl; i++)
+            {
+                Console.WriteLine("podaj kolejny element tablicy");
+                ints[i] = int.Parse(Console.ReadLine());
+            }
+            return ints;
+        }
+                
+
+        }
 }
